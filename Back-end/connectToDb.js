dbc6b4b95da4b1f7d147ffabb75ffc9e0088e59a
DJ <dayanandjagtap07@gmail.com>
2023-08-07 07:17:15 +0530

    Change folder structure
    Seperate frontend and backend

--- Back-end/connectToDb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const uri = "mongodb://127.0.0.1:27017/blogin";
-
-const connectToDb = () => {
-    mongoose.connect(uri).then(() => { console.log("Connected to db") });
-}
-
-module.exports = connectToDb
